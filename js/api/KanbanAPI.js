5233aaa1315099d969a383b2599a30c9c44c14f5
BibhushanKarki <bibhushan.karki11@gmail.com>
2021-10-22 22:47:42 +0545

    update method added

--- js/api/KanbanAPI.js
+++ js/api/KanbanAPI.js
@@ -2,21 +2,21 @@ export default class KanbanAPI {
   static getItems(columnId) {
     const column = read().find(column => column.id == columnId);
 
-    if(!column){
+    if (!column) {
       return [];
     }
     return column.items;
   }
 
-  static InsertItem(columnID,content){
-    const data=read();
+  static InsertItem(columnID, content) {
+    const data = read();
     const column = data.find(column => column.id == columnId);
     const item = {
-      id:Math.floor(Math.random()*100000),
+      id: Math.floor(Math.random() * 100000),
       content
     };
 
-    if(!column){
+    if (!column) {
       throw new Error('Column does not exist');
     }
 
@@ -25,24 +25,44 @@ export default class KanbanAPI {
 
     return item;
   }
+  static updateItems(itemId, newProps) {
+    const data = read();
+
+    // array destructuring 
+
+    const [item, currentColumn] = (() => {
+      for (const column of data) {
+        const item = column.items.find(item => item.id == itemId);
+
+        if (item) {
+          return [item, column];
+        }
+      }
+    })();
+
+    if (!item) {
+      throw new Error("Item not found!");
+    }
+
+    item.content = newProps.content === undefined ? item.content : newProps.content;
+  }
 }
 
-function read(){
-  const json=localStorage.getItem('kanban-data');
+function read() {
+  const json = localStorage.getItem('kanban-data');
 
-  if(!json){
-    return [
-      {
-        id:1,
-        items:[]
+  if (!json) {
+    return [{
+        id: 1,
+        items: []
       },
       {
-        id:2,
-        items:[]
+        id: 2,
+        items: []
       },
       {
-        id:3,
-        items:[]
+        id: 3,
+        items: []
       }
     ];
   }
@@ -52,6 +72,6 @@ function read(){
 
 // saving to local storage
 
-function save(data){
-  localStorage.setItem('kanban-data',JSON.stringify(data));
+function save(data) {
+  localStorage.setItem('kanban-data', JSON.stringify(data));
 }
